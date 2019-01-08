ccb81456900d239b916a81523de12a89a8e8d839
Suarez Santiago <csuarez@indra.es>
2019-01-08 14:58:53 -0600

    Se agregan servicos

--- src/app/search-files/send-mail.service.ts
+++ src/app/search-files/send-mail.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '../core/http.service';
+import { Observable } from 'rxjs/Observable';
+import { SendMailModel } from '../core/models/send-mail.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SendMailService {
 
-  constructor() { }
+  constructor(private http:HttpService) { }
+
+
+  sendMail(data:SendMailModel): Observable<any>{
+    return this.http.post('/scaneadocumentos/enviacorreo',data)
+  }
 }
