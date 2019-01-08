9d28fb6ca1046ef52fd58f11f439168e9247f6f5
Suarez Santiago <csuarez@indra.es>
2019-01-08 14:46:25 -0600

    Se agrega objeto usuario

--- src/app/mailbox/mailbox.service.ts
+++ src/app/mailbox/mailbox.service.ts
@@ -8,11 +8,15 @@ import { UploadFileModel } from '../core/models/upload-file.model';
 })
 export class MailboxService {
 
+  public user ={
+    name:'csuarez'
+  }
+
   constructor(private http:HttpService) { }
 
   public getFileprocesing(data:any): Observable<any> {
     //cambiar esta funcion cuando existan filtros
-    return this.http.get('/scaneadocumentos/estatusprocesamiento?usuario=csuarez')
+    return this.http.get(`/scaneadocumentos/estatusprocesamiento?usuario=${this.user.name}`)
   }
 
 }
